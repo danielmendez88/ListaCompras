8f322ad7ff2ef51bef5904de2decc17fb8f880fe
daniel mendez cruz <danielmendez.88@live.com>
2016-11-21 21:26:33 -0600

    Actualización del Asynctask asi como tambien de el fragment frmcredencial, se actualizo el archivo xml frmcredencial

--- app/src/main/java/app/com/listacompras/asynctask/QRAsyncTask.java
+++ app/src/main/java/app/com/listacompras/asynctask/QRAsyncTask.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -17,43 +19,30 @@ import app.com.listacompras.R;
  * Created by daniel on 20/11/2016.
  */
 
-public class QRAsyncTask extends AsyncTask<String, String, Bitmap> {
-    private Bitmap bitmap;
-    public final static int QRCodeWidth = 500;
-    private ImageView imageview;
+public class QRAsyncTask extends AsyncTask<String, Bitmap, ImageView> {
     Context myContext;
     View viewRoot;
-
+    TextView textView;
+    ImageView imageView;
+    //create values in QR code
+    public final static int QRCodeWidth = 300;
+    Bitmap bitmap;
     public QRAsyncTask(Context context, View root) {
         myContext = context;
         viewRoot = root;
-        imageview = (ImageView) root.findViewById(R.id.QRImageView);
     }
 
-    //method to execute the principal task on a second thread
-    @Override
-    protected Bitmap doInBackground(String... params) {
-            String cadena = params.toString();
-            publishProgress(cadena);
-        return null;
-    }
+    /* method to execute the principal task on a second thread */
 
     @Override
-    protected void onProgressUpdate(String... values) {
-        super.onProgressUpdate(values);
+    protected ImageView doInBackground(String... params) {
         try {
-            bitmap = TextToImageEncode(values.toString());
-            imageview.setImageBitmap(bitmap);
-        }catch (WriterException e)
-        {
+            bitmap = TextToImageEncode(params.toString());
+        } catch (WriterException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    protected void onPostExecute(Bitmap bitmap) {
-        super.onPostExecute(bitmap);
-        imageview.setImageBitmap(bitmap);
+        publishProgress(bitmap);
+        return null;
     }
 
     //first method is execute in Async Task
@@ -62,34 +51,43 @@ public class QRAsyncTask extends AsyncTask<String, String, Bitmap> {
         super.onPreExecute();
     }
 
-    //method to convert text to Image
-     Bitmap TextToImageEncode(String texto) throws WriterException{
-        //declare a matrix
+    @Override
+    protected void onPostExecute(ImageView bitmap) {
+        super.onPostExecute(bitmap);
+    }
+
+    @Override
+    protected void onProgressUpdate(Bitmap... values) {
+        super.onProgressUpdate(values);
+        imageView = (ImageView) viewRoot.findViewById(R.id.QRImageView);
+        imageView.setImageBitmap(values[0]);
+    }
+
+    /* Method to generate Bitmap QR Code*/
+    Bitmap TextToImageEncode(String text) throws WriterException{
+        //declare a Matrix to content bitMap
         BitMatrix bitMatrix;
         try {
-            bitMatrix = new MultiFormatWriter().encode(texto, BarcodeFormat.DATA_MATRIX.QR_CODE, QRCodeWidth,
-                    QRCodeWidth, null);
-        }catch (IllegalArgumentException e)
-        {
-           return null;
-        }
+            bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.DATA_MATRIX.QR_CODE,
+                   QRCodeWidth, QRCodeWidth, null);
+        }catch (IllegalArgumentException e){ return null;}
         //get sizes
-        int bitMatrixWidth = bitMatrix.getWidth();
-        int bitMatrixHeight = bitMatrix.getHeight();
-        //pixel multiple BitMatrixWidth * BitMatrixHeight
-        int pixel[] = new int[bitMatrixWidth * bitMatrixHeight];
-        for (int y= 0; y < bitMatrixHeight; y++)
+        int MatrixWidth = bitMatrix.getWidth();
+        int MatrixHeight = bitMatrix.getHeight();
+        // initiation of pixel
+        int[] pixel = new int[MatrixWidth * MatrixHeight];
+        //generate the loop to create the bitmap
+        for (int y = 0; y < MatrixHeight; y++)
         {
-            int offset =  y * bitMatrixWidth;
-            for (int x = 0; x < bitMatrixWidth; x++)
+            int offset = y * MatrixWidth;
+            for (int x = 0; x < MatrixWidth; x++)
             {
-                pixel[offset + x] = bitMatrix.get(x, y) ? myContext.getResources().getColor(R.color.QRCodeBlackColor):myContext.getResources()
-                        .getColor(R.color.QRCodeWhiteColor);
+                pixel[offset + x] = bitMatrix.get(x, y) ? myContext.getResources().getColor(R.color.QRCodeBlackColor)
+                        :myContext.getResources().getColor(R.color.QRCodeWhiteColor);
             }
         }
-        //
-        Bitmap bMap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_8888);
-        bMap.setPixels(pixel, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        Bitmap bMap = Bitmap.createBitmap(MatrixWidth, MatrixHeight, Bitmap.Config.ARGB_8888);
+        bMap.setPixels(pixel, 0, 300, 0, 0, MatrixWidth, MatrixHeight);
         return bMap;
     }
 }
