d279ba1f259107a0b4ea360618bd3a64562bea3e
daniel mendez cruz <danielmendez.88@live.com>
2017-03-01 01:32:12 -0600

    cambios en la función de pseudo aleatorios generados

--- app/src/main/java/app/com/listacompras/clases/Prngenerator.java
+++ app/src/main/java/app/com/listacompras/clases/Prngenerator.java
@@ -1,6 +1,8 @@
 package app.com.listacompras.clases;
 
 
+import android.util.Log;
+
 import java.util.Random;
 
 /**
@@ -10,17 +12,17 @@ import java.util.Random;
 public class Prngenerator{
 
     private long seed_generator;
-    private int ci_generator;
+    private int length_generator;
     // private int a[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
 
     /**
      *
      * @param seed seed value
-     * @param cicles iterations' numbers that I want to generate
+     * @param length iterations' numbers that I want to generate
      */
-    public Prngenerator(long seed, int cicles) {
+    public Prngenerator(long seed, int length) {
         this.seed_generator = seed;
-        this.ci_generator = cicles;
+        this.length_generator = length;
         //int start = 1, end = 1;
         //start = a[seed_generator-1];
         //end = a[seed_generator];
@@ -29,16 +31,19 @@ public class Prngenerator{
     public long generator()
     {
         //max for digits = 8, max will be 99999999
-        int max = (int) Math.pow(10, (ci_generator)) - 1;
+        int max = (int) Math.pow(10, length_generator) - 1;
         //for digits = 8, min will be 10000000
-        int min = (int) Math.pow(10, ci_generator-1);
+        int min = (int) Math.pow(10, length_generator - 1);
         //range
         int range = max-min;
 
+        //create a random object
         Random rand = new Random();
         rand.setSeed(seed_generator);
-        int x = rand.nextInt(range);
-        return x+min;
+        //setting seed
+        int x = rand.nextInt(range + 1) + min;
+        Log.d("random: ", String.valueOf(rand.nextInt()));
+        return x;
     }
 
 }
