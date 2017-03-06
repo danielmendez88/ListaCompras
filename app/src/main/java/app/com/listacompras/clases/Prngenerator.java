69b9354c30868a55734f24222f4a8e6e4f036870
daniel mendez cruz <danielmendez.88@live.com>
2017-03-06 12:52:19 -0600

    último cambio en el método generar aleatorio

--- app/src/main/java/app/com/listacompras/clases/Prngenerator.java
+++ app/src/main/java/app/com/listacompras/clases/Prngenerator.java
@@ -4,7 +4,6 @@ package app.com.listacompras.clases;
 import android.util.Log;
 
 import java.util.LinkedList;
-import java.util.Random;
 
 /**
  * daniel Mendez Cruz on 25/02/2017. Created By DMC
@@ -16,7 +15,7 @@ public class Prngenerator{
     private StringBuilder stb = new StringBuilder();
     private long seed_generator;
     private int length_generator;
-    private int a[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+    private int a[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000}; //general array
 
     /**
      *
@@ -26,47 +25,30 @@ public class Prngenerator{
     public Prngenerator(long seed, int length) {
         this.seed_generator = seed;
         this.length_generator = length;
-        //int start = 1, end = 1;
-        //start = a[seed_generator-1];
-        //end = a[seed_generator];
-
-    }
-    public long generator()
-    {
-        //max for digits = 8, max will be 99999999
-        int max = (int) Math.pow(10, length_generator) - 1;
-        //for digits = 8, min will be 10000000
-        int min = (int) Math.pow(10, length_generator - 1);
-        //range
-        int range = max-min;
-
-        //create a random object
-        Random rand = new Random();
-        rand.setSeed(seed_generator);
-        //setting seed
-        int x = rand.nextInt(range) + min;
-        Log.d("random: ", String.valueOf(x));
-        return x;
     }
 
+    /**
+     *
+     * @return a long value that send a type int data to textView
+     */
     public long random_generator()
     {
-        int limit = 6;
-        long set_value;
-        long z = (long) Math.pow(seed_generator, 2);
-        int trim = (length_generator/2);
+        int limit = 6; //limit of numbers that the array can stores
+        long set_value; //this is the value we can set as module
+        long z = (long) Math.pow(seed_generator, 2); //long value to raise seed to square
+        int trim = (length_generator/2); // in this case the value gets the result that divide length generator between 2
         z = z/a[trim];
         long n = z%10;
-        new_list.addFirst(n);
+        new_list.addFirst(n); // add into the list array but we add the current element first
         Log.d("actual valor", String.valueOf(n));
-        for (int j = 0; j < length_generator; j++)
+        for (int j = 0; j < length_generator; j++) //Main loop to get the middle values
         {
             Log.d("next num" + String.valueOf(j), String.valueOf((z%(a[trim]))*(a[j])));
              z /= 10;
             if (j <= limit)
             {
-                set_value = z%10;
-                new_list.addFirst(set_value);
+                set_value = z%10; //set module and assign the value to it
+                new_list.addFirst(set_value); //
                 Log.d("division values" + String.valueOf(j) + ":", String.valueOf(set_value));
             }
         }
@@ -82,5 +64,3 @@ public class Prngenerator{
 
 
 }
-
-//http://stackoverflow.com/questions/34049996/how-can-i-check-if-two-strings-have-the-same-letters-but-only-print-the-common
